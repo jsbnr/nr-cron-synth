970d05d8417258e9145d722b45e6717aeafb6fee
James Buchanan <jbuchanan@newrelic.com>
2022-03-04 16:34:43 +0000

    init

---
+++ cron.js
@@ -0,0 +1,333 @@
+const ACCOUNT_ID = "0"  // You account ID
+const REGION = "US"     // US or EU
+const BLOCKSIZE = 10    //minutes - you shold ensure you monitor is executed at least once within this time period.
+
+const NAMESPACE="cronsynth" //Adjust this if you're running multiple copies of this monitor and dont want them to clash. Each monitor should use unique value.
+
+//Add your jobs to this array
+const JOBS = [
+    {
+        name: "Job 1",
+        cron: {
+            months: [3],            // 1 (Jan) 2 ... 11 12 (Dec)
+            dayOfMonth: [],         // 1 2 ... 30 31
+            dayOfWeek: [],          // 1 (Monday) 2 ... 6 7 (Sunday)
+            hourOfDay: [],          // 0 1 ... 22 23
+            minuteOfHour: [10,30]   //the values here must be multiples of BLOCKSIZE!
+        },
+        fn: async ()=>{ 
+            await sleep(5000); 
+            console.log("Job 1 ran!")
+        }
+    },
+    {
+        name: "Job 2",
+        cron: {
+            months: [],             //all empty means it will execute on every run!
+            dayOfMonth: [],
+            dayOfWeek: [],
+            hourOfDay: [],
+            minuteOfHour: []
+        },
+        fn: async ()=>{ 
+            await sleep(5000);
+            console.log("Job 2 ran!")
+        }
+    }
+]
+
+
+
+/*
+You shouldnt need to configure anything below here!
+*/
+
+const CRON_SYNTH_VERSION="1.0.0" 
+const DEFAULT_TIMEOUT = 10000 //timeout on http requests
+let RUNNING_LOCALLY=false
+
+/*
+*  ========== LOCAL TESTING CONFIGURATION ===========================
+* This is used if running from local laptop rather than a minion
+*/
+const IS_LOCAL_ENV = typeof $http === 'undefined';
+if (IS_LOCAL_ENV) {  
+    RUNNING_LOCALLY=true
+    var $http = require("request"); 
+    var $secure = {}                    
+    var $env = {}
+    $env.MONITOR_ID="local"
+    $env.JOB_ID="0"
+
+    //When testing ONLY set you API keys here
+    $secure.CS_INSERT_KEY = "XXXNRAL"  ///...NRAL
+    $secure.CS_QUERY_KEY = "NRAK-XXX" //NRAK...
+
+    console.log("Running in local mode")
+} 
+
+let moment = require('moment');
+let assert = require('assert');
+
+let INSERT_KEY = $secure.CS_INSERT_KEY
+let QUERY_KEY = $secure.CS_QUERY_KEY
+
+const GRAPHQL_URL= REGION=="US" ? "https://api.newrelic.com/graphql" : "https://metric-api.eu.newrelic.com/graphql"
+
+/*
+*  ========== SOME HELPER FUNCTIONS ===========================
+*/
+
+
+/*
+* asyncForEach()
+*
+* A handy version of forEach that supports await.
+* @param {Object[]} array     - An array of things to iterate over
+* @param {function} callback  - The callback for each item
+*/
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+  
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*
+* genericServiceCall()
+* Generic service call helper for commonly repeated tasks
+*
+* @param {number} responseCodes  - The response code (or array of codes) expected from the api call (e.g. 200 or [200,201])
+* @param {Object} options       - The standard http request options object
+* @param {function} success     - Call back function to run on successfule request
+*/
+const  genericServiceCall = function(responseCodes,options,success) {
+    !('timeout' in options) && (options.timeout = DEFAULT_TIMEOUT) //add a timeout if not already specified 
+    let possibleResponseCodes=responseCodes
+    if(typeof(responseCodes) == 'number') { //convert to array if not supplied as array
+      possibleResponseCodes=[responseCodes]
+    }
+    return new Promise((resolve, reject) => {
+        $http(options, function callback(error, response, body) {
+        if(error) {
+            reject(`Connection error on url '${options.url}'`)
+        } else {
+            if(!possibleResponseCodes.includes(response.statusCode)) {
+                let errmsg=`Expected [${possibleResponseCodes}] response code but got '${response.statusCode}' from url '${options.url}'`
+                reject(errmsg)
+            } else {
+                resolve(success(body,response,error))
+            }
+          }
+        });
+    })
+  }
+
+/*
+* setAttribute()
+* Sets a custom attribute on the synthetic record
+*
+* @param {string} key               - the key name
+* @param {Strin|Object} value       - the value to set
+*/
+const setAttribute = function(key,value) {
+    if(!RUNNING_LOCALLY) { //these only make sense when running on a minion
+        $util.insights.set(key,value)
+    } else {
+        console.log(`Set attribute '${key}' to ${value}`)
+    }
+}
+
+
+/*
+* sendDataToNewRelic()
+* Sends a metrics payload to New Relic
+*
+* @param {object} data               - the payload to send
+*/
+const sendDataToNewRelic = async (data) =>  {
+    let request = {
+        url: "https://metric-api.newrelic.com/metric/v1",
+        method: 'POST',
+        json: true,
+        headers :{
+            "Api-Key": INSERT_KEY
+        },
+        body: data
+    }
+    console.log("\nSending data to NR metrics API...")
+    return genericServiceCall([200,202],request,(body,response,error)=>{
+        if(error) {
+            log(`NR Post failed : ${error} `,true)
+            return false
+        } else {
+            return true
+        }
+        })
+}
+
+const checkRecentRun = async (timeBlock) => {
+    const graphQLQuery=`{
+        actor {
+          account(id: ${ACCOUNT_ID}) {
+            nrql(query: "select count(*) from Metric where ${NAMESPACE}.timeBlock='${timeBlock}' since 1 hour ago") {
+              results
+            }
+          }
+        }
+      }
+      `
+    const options =  {
+            url: GRAPHQL_URL,
+            method: 'POST',
+            headers :{
+              "Content-Type": "application/json",
+              "API-Key": QUERY_KEY
+            },
+            body: JSON.stringify({ "query": graphQLQuery})
+        }
+
+
+    let body =  await genericServiceCall([200],options,(body)=>{return body})
+
+    try {
+        bodyJSON = JSON.parse(body)
+        let recentRuns=bodyJSON.data.actor.account.nrql.results[0].count
+        if(!recentRuns) {
+            console.log(`Recent run for this timeblock (${timeBlock}) not detected`)
+            return false
+        } else {
+            console.log(`${recentRuns} Recent runs were detected for this time block (${timeBlock})`)
+            return true
+        }
+        
+    } catch(e) {
+        console.log("Error: Response from New Relic could not be parsed",e)
+        assert.fail(e)
+    }
+}
+
+
+//creates a simple timeblock reference HH:m rounded to the blocksize
+const genTimeBlock = (time) => {
+    const timeBlock = time.format("HH") + ":" + Math.floor(parseInt(time.format("m")) / BLOCKSIZE) * BLOCKSIZE
+    console.log(`This ${BLOCKSIZE}min time block: ${timeBlock}`)
+    return timeBlock
+}
+
+
+//Records the script running in NRDB
+const recordRun = async (timeBlock,numJobs) => {
+    let commonMetricBlock={"attributes": {}}
+    commonMetricBlock.attributes[`${NAMESPACE}.timeBlock`]=timeBlock
+    commonMetricBlock.attributes[`${NAMESPACE}.monitorId`]=$env.MONITOR_ID
+    commonMetricBlock.attributes[`${NAMESPACE}.jobId`]=$env.JOB_ID
+    commonMetricBlock.attributes[`cronsynth.version`]=CRON_SYNTH_VERSION
+ 
+    let metricsPayLoad=[{ 
+        "common" : commonMetricBlock,
+        "metrics": [{
+            name: `${NAMESPACE}.value`,
+            type: "gauge",
+            value: numJobs,
+            timestamp: Math.round(Date.now()/1000)
+        }]
+    }]
+
+    console.log(`Logging ${numJobs} jobs run`)
+    let NRPostStatus = await sendDataToNewRelic(metricsPayLoad)
+    if( NRPostStatus === true ){
+        setAttribute("nrPostStatus","success")
+        console.log("NR Post successful")   
+    } else {
+        setAttribute("nrPostStatus","failed")
+        console.log("NR Post failed")   
+    }
+}
+
+//runs the jobs nased on their settings
+async function jobRunner(timeNow) {
+    let jobsTriggered=0
+    const month = parseInt(timeNow.format("M")) //1 2 ... 11 12
+    const dayOfMonth = parseInt(timeNow.format("D")) //1 2 ... 30 31
+    const dayOfWeek = parseInt(timeNow.format("E")) //1 (mon) 2 ... 6 7 (sun) 
+    const hourOfDay = parseInt(timeNow.format("H")) //0 1 ... 22 23
+    const minuteOfHour = parseInt(Math.floor(parseInt(timeNow.format("m")) / BLOCKSIZE) * BLOCKSIZE)
+    console.log(`Now: month: ${month}, dayOfMonth ${dayOfMonth}, dayOfWeek:${dayOfWeek}, hourOfDay:${hourOfDay}, minuteOfDay:${minuteOfHour}`)
+
+    await asyncForEach(JOBS,async (job)=>{
+
+        if(job.cron) {
+            let executeJob = true
+
+            //months
+            if(job.cron.months && job.cron.months.length > 0 ) {
+                executeJob = job.cron.months.includes(month)
+            }
+            //dayOfMonth
+            if(job.cron.dayOfMonth && job.cron.dayOfMonth.length > 0 ) {
+                executeJob = job.cron.dayOfMonth.includes(dayOfMonth)
+            }
+            //dayOfWeek
+            if(job.cron.dayOfWeek && job.cron.dayOfWeek.length > 0 ) {
+                executeJob = job.cron.dayOfWeek.includes(dayOfWeek)
+            }
+            //hourOfDay
+            if(job.cron.hourOfDay && job.cron.hourOfDay.length > 0 ) {
+                executeJob = job.cron.hourOfDay.includes(hourOfDay)
+            }
+            //minuteOfHour
+            if(job.cron.minuteOfHour && job.cron.minuteOfHour.length > 0 ) {
+                executeJob = job.cron.minuteOfHour.includes(minuteOfHour)
+            }
+
+            if(executeJob){
+                jobsTriggered++
+                console.log(`Execute job: ${job.name}`)
+                await job.fn()
+                console.log(`Finished job: ${job.name} `)
+            }
+        }
+    })
+    return jobsTriggered
+}
+
+
+
+async function cronRunner()  {
+    let timeNow = moment().utc()
+    let timeBlock=genTimeBlock(timeNow)
+    const recentRuns = await checkRecentRun(timeBlock)
+
+    if(recentRuns) {
+        console.log("This timeblock has run already, skipping jobs")
+        setAttribute("jobsSkipped","Yes")
+        setAttribute("jobsTriggered",0)
+    } else {
+        setAttribute("jobsSkipped","No")
+        const jobsTriggered = await jobRunner(timeNow)
+        setAttribute("jobsTriggered",jobsTriggered)
+        await recordRun(timeBlock,jobsTriggered)
+    }
+    
+    return true
+}
+
+
+try {
+    cronRunner()
+    .then((success)=>{
+        
+        if(success === true ) {
+            console.log("Completed successfully")
+        } else {
+            console.log("Completed with errors")
+        }
+    })
+} catch(e) {
+    console.log("Unexpected errors: ",e)
+}
---
+++ readme.md
@@ -0,0 +1,92 @@
+# CronSynth - Cronlike jobs in New Relic Synthetics
+This project provides a easy to use scaffold for running any sort of job (http check, api request, etc) within the New Relic synhtetic minon whilst controlling the times job runs using cron-like configuration. You can set jobs to run on a specified month, day of month, day of week, hour, min or any combination.
+
+A job is simply a javascript function that runs at configured times, what it does is left up to you. For instance you could use this to adjust alert thresholds at certain times of day or initiate a weekly report.
+
+
+## How it works
+The monitor needs to be run on a regular basis, say every 5 minutes. For the given time block (default is 10 minutes) the jobs satisfying the cron configuration are executed. The monitor records that the jobs have been executed for that time block. If the monitor runs again then jobs will only be executed if a previous execution for the same block was not detected. Or to put this another way, with a block size of 10 minutes jobs can at  execute at most every 10 minutes.
+
+
+## Initial setup
+The monitor uses NRDB as a data store to record successful exectutions. You will need an INGEST key and a USER key in order to allow read and write to NRDB. These should be provided as **secure credentials**:
+
+- **CS_INSERT_KEY:** A New Relic Ingest key (xxxxNRAL)
+- **CS_QUERY_KEY:**  A New Relic User key for querying data (NRAKxxxx)
+
+Next you must configure the following at the top of the script:
+
+- ACCOUNT_ID: The account ID relating to the keys above, where the data will be stored
+- REGION: Either `US` or `EU` depending on where your New Relic account is hosted
+- BLOCKSIZE: Size of a cron time block in minutes. Default is 10 (5, 15, 30 are other sensible alternatives). Important: you must set you monitor to run at least once within this period otherwise blocks might get missed. A block size less than 5 is not recommended.
+
+**Optional:**
+If you are running more that one copy of this within an account you can ensure they do not conflict by providing a unique `NAMESPACE` value.
+
+
+## Configuring Jobs
+Jobs are simply javascript functions that do something. Ensure they are async/await'able. Jobs are specified in an array and processed in turn. For each job you'll specify the job name, its cron config and the function to call. 
+
+#### Job Configuration
+- **name:** the name of your job
+- **cron:** the cron configuration for your job (see below)
+- **fn:** the async function to call for you job (you need to write this!)
+
+
+#### Cron configuration
+For each cron option you can provide an empty array or omit the attribute entirely to run at all times - like the asteric (*) in cron. Each should be an array of values to match.
+- **months:**  `1 (Jan) 2 ... 11 12 (Dec)`
+- **dayOfMonth:** `1 2 ... 30 31`
+- **dayOfWeek:** `1 (Monday) 2 ... 6 7 (Sunday) `
+- **hourOfDay:** `0 1 ... 22 23`
+- **minuteOfHour:** Depends on block size. For block size of 10 valid values are `0,10,20...50`. For block size 5 valid values are `0,5,15...50,55` etc.
+
+### Example
+In this example there are two jobs configured. In this case all they do is sleep and log to console. 
+
+Job 1 will run only:
+- In the months of January, February and March 
+- AND only the 1st and 15th of the those months
+- AND on those days on the top and half of every hour
+
+
+Job 2 will run only:
+ - Monday to Friday
+ - At 9.30 am
+
+
+```javascript
+const JOBS = [
+    {
+        name: "Job 1",
+        cron: {
+            months: [1,2,3],
+            dayOfMonth: [1,15],
+            dayOfWeek: [],
+            hourOfDay: [],
+            minuteOfHour: [0,30]
+        },
+        fn: async ()=>{ 
+            await sleep(5000); 
+            console.log("Job 1 ran!")
+        }
+    },
+    {
+        name: "Job 2",
+        cron: {
+            months: [],
+            dayOfMonth: [],
+            dayOfWeek: [1,2,3,4,5],
+            hourOfDay: [9],
+            minuteOfHour: [30]
+        },
+        fn: async ()=>{ 
+            await sleep(5000);
+            console.log("Job 2 ran!")
+        }
+    }
+]
+```
+
+## Local Testing
+You can test locally by running `npm install` and then simply running `node cron.js`. Be sure to set your API keys in the local setup section where indicated.
